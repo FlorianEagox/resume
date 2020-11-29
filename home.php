625afa91aaa67e3ea12e8989f33d8a2102989ceb
seth <seth@sethpainter.com>
2020-11-29 08:03:31 +0000

    split markup to seperate file so it can be easily accessed from the website.

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Resume - Seth Painter</title>
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Arimo" />
+	<script src="https://kit.fontawesome.com/c3db8f4cfe.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+	<?php require('resume.html') ?>
+</body>
+
+</html>
